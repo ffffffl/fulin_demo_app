c31502b066d9683ae1ad2da60b867f45f4d09f40
ffffffl <65761562+ffffffl@users.noreply.github.com>
2021-10-19 09:18:57 +0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -25,10 +25,9 @@ def get_choices(col: str):
 class ImoForm(forms.Form):
     imo = forms.IntegerField(label='IMO Number', min_value=1111111, max_value=9999999)
     ship_name = forms.CharField(max_length=64)
-    ship_type = forms.CharField(max_length=64)
     technical_efficiency_number = forms.DecimalField(label='EEDI', max_digits=6, min_value=0, required=False)
-    issue_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), required=False)
-    expiry_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), required=False)
+#     issue_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), required=False)
+#     expiry_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), required=False)
     # my_choice_field = forms.ChoiceField(choices=get_choices('col'), required=False)
     # my_date_field = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}), required=False)
     
