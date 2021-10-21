f142ccf44837d63968b826c4d17c3831eca6228f
ffffffl <65761562+ffffffl@users.noreply.github.com>
2021-10-21 15:32:22 +0800

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('emissions/imo/<int:imo>', app.views.emission_detail, name='emission_detail'),
     path('admin/', admin.site.urls),
     path('aggregation/',app.views.aggregation,name='aggregation'),
-    path('visualisation/', app.views.visualisation, name='visualisation')
+    path('visual/', app.views.visual, name='visual')
 
 ]
 
